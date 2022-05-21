11d9c3b619c6a61d1b4d0b816a54790c1dcd1a7d
bradcarthew1 <88962237+bradcarthew1@users.noreply.github.com>
2022-05-21 15:07:49 +0200

    Update audio_gen.v

--- Group_6_YODA_project/Group_6_YODA_project.srcs/sim_1/new/audio_gen.v
+++ Group_6_YODA_project/Group_6_YODA_project.srcs/sim_1/new/audio_gen.v
@@ -1,15 +1,15 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: University of Cape Tonw
+// Engineer: Bradley Carthew, Nathanael Thomas, Thomas Stern, Mohammed-Bilaal Sheik Hoosen
 // 
 // Create Date: 05/19/2022 09:55:57 AM
-// Design Name: 
+// Design Name: Audio Generator
 // Module Name: audio_gen
-// Project Name: 
+// Project Name: EEE4121F YODA Project
 // Target Devices: 
 // Tool Versions: 
-// Description: 
+// Description: A module used to generate individual audio samples from an audio file.
 // 
 // Dependencies: 
 // 
@@ -21,36 +21,38 @@
 
 
 module audio_gen(
-    input clk ,
-    input wire [1:0] interval,
-    output reg enable,
-    output reg done,
-    output reg [31:0] sine_1, sine_2, sine_3
+    input clk , //clock line
+    input wire [1:0] interval, //number of intervals
+    output reg enable, //enable line for min_max module
+    output reg done, //done line for std_dev module
+    output reg [31:0] sine_1, sine_2, sine_3 //output line providing audio samples to min_max module
     );
-    
-parameter SIZE = 441000;  
-reg [31:0] rom_memory [SIZE-1:0];
-integer i,j,k,l;
 
+parameter SIZE = 441000;  //initialises the size of the audio file
+reg [31:0] rom_memory [SIZE-1:0]; //defines the register to store audio file samples
+integer i,j,k,l; //defines the rom position variables
+
+//initial block 
 initial begin
-    $readmemh("ImperialMarch.mem", rom_memory);
-    i = 0;
-    j = 0;
+    $readmemh("ImperialMarch.mem", rom_memory); //initialises the rom memory with an audio file
+    //initialising the rom position variables
+    i = 0;  
+    j = 0; //used for the enable line
     l = 0;
     k = 0;
     
-    if (interval == 2) begin
-        k = (SIZE/2);
-    end else if (interval == 3) begin       
-        l = (SIZE/3)*2;
-        k = (SIZE/3);
+    if (interval == 2) begin //if there are two interval segments
+        k = (SIZE/2); //initialises the starting position of the second rom iteration
+    end else if (interval == 3) begin  //if there are three interval segments      
+        l = (SIZE/3)*2; //initialises the starting position of the third rom iteration
+        k = (SIZE/3); //initialises the starting position of the second rom iteration
     end
     
-    enable = 1'b0;
-    done = 1'b0;
+    enable = 1'b0; //set the enable line to a logic low
+    done = 1'b0; //set the done line to a logic low
 end
     
-//At every positive edge of the clock, output a sine wave sample.
+//at every positive edge of the clock, output a sample from the audio file
 always@(posedge clk) begin
     if (interval == 1) begin
         sine_1 = rom_memory[i];
